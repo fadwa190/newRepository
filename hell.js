e6985e046866c7034f18d098d09157feb1ad66cc
NehalAedh <117654725+NehalAedh@users.noreply.github.com>
2022-11-09 13:51:52 +0300

    Update hell.js

--- hell.js
+++ hell.js
@@ -1,6 +1,6 @@
-var http = require('http');
+var http = require('httP');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('hello world!');
+  res.end('Hello World!:)');
 }).listen(8080);
